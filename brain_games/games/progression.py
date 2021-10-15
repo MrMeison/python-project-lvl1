83875e82a937f31e9eac0e96dcab7be6a165c72f
Konstantin Mamaev <konstantin.mamaev@abbyy.com>
2021-10-15 16:36:32 +0300

    complete 8 step

---
+++ brain_games/games/progression.py
@@ -0,0 +1,24 @@
+from random import randint, choice
+
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+def get_progression(start, step, length):
+    items = []
+    for i in range(length):
+        items.append(start + i * step)
+
+    return items
+
+def generate_round():
+    start = randint(-100, 100)
+    step = randint(1, 100)
+    length = randint(5, 10)
+
+    progression = get_progression(start, step, length)
+
+    hidden_index = randint(0, length - 1)
+    answer = str(progression[hidden_index])
+    progression[hidden_index] = '..'
+    question = ' '.join([str(item) for item in progression])
+
+    return question, answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,8 @@
+from brain_games.endpoint import run
+import brain_games.games.progression as progression
+
+def main():
+    run(progression)
+
+if __name__ == '__main__':
+    main()
