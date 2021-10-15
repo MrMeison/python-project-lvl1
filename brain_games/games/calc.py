20f033bdd1ded884266b6dc428e0494c0f451c23
Konstantin Mamaev <konstantin.mamaev@abbyy.com>
2021-10-15 15:54:17 +0300

    complete 6 step

---
+++ brain_games/games/calc.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+
+DESCRIPTION = 'What is the result of the expression?'
+OPERATORS = ('+', '-', '*')
+
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+
+def generate_round():
+    num1 = randint(0, 100)
+    num2 = randint(0, 100)
+    operator = choice(OPERATORS)
+    question = '{} {} {}'.format(num1, operator, num2)
+    answer = str(calculate(num1, num2, operator))
+
+    return question, answer
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,8 @@
+from brain_games.endpoint import run
+import brain_games.games.calc as calc
+
+def main():
+    run(calc)
+
+if __name__ == '__main__':
+    main()
